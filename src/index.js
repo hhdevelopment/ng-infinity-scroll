857852a80be3eb2ba6f83ef884a36833f6287d9c
Francois Achache <francois.achache@gmail.com>
2018-01-31 23:03:15 +0100

    improve computing limit

--- src/index.js
+++ src/index.js
@@ -11,12 +11,10 @@ import './infinityscroll.js';
 	ng.module('app', ['infinity.scroll']).controller('AppCtrl', AppCtrl);
 	function AppCtrl() {
 		var ctrl = this;
-		ctrl.categories = [{'name':'Directory 1', nb:5},{'name':'Directory 2', nb:200},{'name':'Directory 3', nb:20},{'name':'Directory 4', nb:0}];
+		ctrl.categories = [{'name':'Directory 1', nb:5},{'name':'Directory 2', nb:20000},{'name':'Directory 3', nb:20},{'name':'Directory 4', nb:0}];
 		ctrl.selectedCategory = null;
 		ctrl.selectCategory = selectCategory;
 		ctrl.items = null;
-		ctrl.limit = 10;
-		ctrl.begin = 0;
 		
 		function selectCategory(cat) {
 			ctrl.selectedCategory = cat;
